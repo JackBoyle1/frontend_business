c3b519c8c956c30aca5e241ece2b227f99c652f1
Jack <jackboyle35@gmail.com>
2023-11-09 13:23:40 +0000

    add proxy for backend server

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from 'nuxt/config'
 export default defineNuxtConfig({
   app: {
     head: {
-      title: 'Boyx | ',
-      charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1',
-    }
+      title: "Boyx | ",
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+    },
+  },
+  nitro: {
+    devProxy: {
+      "/api/": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+        prependPath: true,
+      },
+    },
   },
   devtools: { enabled: true },
-  modules: [
-    '@nuxtjs/tailwindcss'
-  ]
+  modules: ["@nuxtjs/tailwindcss"],
 });
