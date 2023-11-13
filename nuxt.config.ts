cec8e2bf25d19d17cecab18d47946a3371c87257
Jack <jackboyle35@gmail.com>
2023-11-13 23:43:36 +0000

    update nuxt config meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,9 @@ export default defineNuxtConfig({
   app: {
     head: {
       title: "Boyx | ",
+      meta: [
+        { hid: 'Boyx', name: 'Boyx', content: 'Your Partner in Web Development' },
+      ],
       charset: "utf-8",
       viewport: "width=device-width, initial-scale=1",
     },
