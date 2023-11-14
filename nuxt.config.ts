c919e325f371fa17843b76396c1569a8aedc771b
Jack <jackboyle35@gmail.com>
2023-11-14 00:07:01 +0000

    generate robots.txt on build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,33 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import { defineNuxtConfig } from 'nuxt/config'
+import { defineNuxtConfig } from "nuxt/config";
 export default defineNuxtConfig({
   app: {
     head: {
       title: "Boyx | ",
       meta: [
-        { hid: 'Boyx', name: 'Boyx', content: 'Your Partner in Web Development' },
+        {
+          hid: "Boyx",
+          name: "Boyx",
+          content: "Your Partner in Web Development",
+        },
       ],
       charset: "utf-8",
       viewport: "width=device-width, initial-scale=1",
     },
   },
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@pinia/nuxt",
+    [
+      "@nuxtjs/robots",
+      {
+        rules: {
+          UserAgent: "*",
+          Disallow: "/thankyou",
+          Allow: "*",
+        },
+      },
+    ],
+  ],
 });
